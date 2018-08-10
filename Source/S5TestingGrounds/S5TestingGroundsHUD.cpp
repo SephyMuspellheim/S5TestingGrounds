7d718b4425f379b70144cdb3d5b5f4f47e4c2e8f
SephyMuspellheim <37329992+SephyMuspellheim@users.noreply.github.com>
2018-08-10 14:45:48 +0100

    Updated Content Browser folder structure and removed all unnecessary files and assets and reorganised existing ones and provided more intuitive file hierarchy

--- Source/S5TestingGrounds/S5TestingGroundsHUD.cpp
+++ Source/S5TestingGrounds/S5TestingGroundsHUD.cpp
@@ -10,7 +10,7 @@
 AS5TestingGroundsHUD::AS5TestingGroundsHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
